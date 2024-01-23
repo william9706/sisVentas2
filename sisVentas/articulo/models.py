8ea2bc3032b0b567c72a8432da0f3ab02d32da88
william9706 <wagnerveleswilliam@gmail.com>
2024-01-22 22:52:42 -0500

    Agrega nuevo modulo articulo y un modelo llamado Articulo

--- sisVentas/articulo/models.py
+++ sisVentas/articulo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
 # Create your models here.
+
+
+class Categoria(TimeStampedModel):
+    """
+    categorias' model
+    """
+    nombre = models.CharField(_("Nombre del articulo"), max_length=25)
+    descripcion = models.CharField(_("Descripcion del articulo"), max_length=50)
+    condicion = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _("Categoria")
+        verbose_name_plural = _("Categorias")
+
+    def __str__(self):
+        return self.nombre
