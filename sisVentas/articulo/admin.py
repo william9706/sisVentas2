92501c8c5e6356ba3bea90fb5e195c26e11ee456
william9706 <wagnerveleswilliam@gmail.com>
2024-01-23 18:13:20 -0500

    Agrega modelos categoria y articulos para aplicacion articulo ademas de agregar al requirements las librerias de flake8, isort, black y pillow

--- sisVentas/articulo/admin.py
+++ sisVentas/articulo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from sisVentas.articulo.models import Categoria, Articulo
 
-# Register your models here.
+
+@admin.register(Categoria)
+class Categoria(admin.ModelAdmin):
+    """
+    Admin for model Admin.
+    """
+
+@admin.register(Articulo)
+class Articulo(admin.ModelAdmin):
+    """
+    Admin for model Admin.
+    """
+    list_display = ('nombre', 'codigo', 'stock')
--- sisVentas/articulo/models.py
+++ sisVentas/articulo/models.py
@@ -1,15 +1,18 @@
+from collections.abc import Sequence
+from typing import Tuple
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
-# Create your models here.
 
 
 class Categoria(TimeStampedModel):
     """
-    categorias' model
+    categoria's model
     """
-    nombre = models.CharField(_("Nombre del articulo"), max_length=25)
-    descripcion = models.CharField(_("Descripcion del articulo"), max_length=50)
+
+    nombre = models.CharField(_("Nombre de la categoria"), max_length=25)
+    descripcion = models.CharField(_("Descripcion de la categoria"), max_length=50)
     condicion = models.BooleanField(default=False)
 
     class Meta:
@@ -18,3 +21,58 @@ class Categoria(TimeStampedModel):
 
     def __str__(self):
         return self.nombre
+
+
+class Articulo(TimeStampedModel):
+    """
+    Article model for managing article data.
+    """
+
+    class EstadoFactura(models.IntegerChoices):
+        ERROR: Tuple[int, Sequence[str]] = 1, _("Error en la factura")
+        PRESENTADA: Tuple[int, Sequence[str]] = 2, _("Factura radicada")
+        BORRADOR: Tuple[int, Sequence[str]] = 3, _("Factura sin radicar")
+
+    categoria = models.ForeignKey(
+        Categoria,
+        verbose_name=_("Categoria"),
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+    )
+    codigo = models.CharField(
+        _("Codigo"),
+        null=False,
+        blank=False,
+        max_length=50,
+    )
+    nombre = models.CharField(
+        _("Nombre"),
+        null=False,
+        blank=False,
+        max_length=150,
+    )
+    stock = models.IntegerField(
+        verbose_name=_("Stock"),
+        null=False,
+        blank=False,
+    )
+    descripcion = models.TextField(
+        _("Descripcion"),
+        null=True,
+        blank=True,
+        max_length=500,
+    )
+    imagen = models.ImageField(upload_to="media/articulos", blank=True, null=True)
+    estado = models.PositiveSmallIntegerField(
+        _("Estado del articulo"),
+        choices=EstadoFactura.choices,
+        default=EstadoFactura.BORRADOR,
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = _("Articulo")
+        verbose_name_plural = _("Articulos")
