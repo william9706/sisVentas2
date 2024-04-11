1b77c379e66b25b0c3c3b6681d0ef7af430db425
william9706 <wagnerveleswilliam@gmail.com>
2024-04-11 17:21:56 -0500

    Agrega pruebass para probar metodos post y get falta mejorar estos 2 test para validar informacion creada

--- sisVentas/articulo/api/serializers.py
+++ sisVentas/articulo/api/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 
-from sisVentas.articulo.models import Articulo
+from sisVentas.articulo.models import Articulo, Categoria
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    """
+    Serializador para modelo categoria.
+    """
+
+    class Meta:
+        model = Categoria
+        fields = "__all__"
 
 
 class ArticuloSerializer(serializers.ModelSerializer):
--- sisVentas/articulo/tests/test_api.py
+++ sisVentas/articulo/tests/test_api.py
@@ -1,8 +1,10 @@
 import pytest
 from django.contrib.auth import get_user_model
-from django.test import Client
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
+
+from sisVentas.articulo.models import Articulo
 
 pytestmark = pytest.mark.django_db
 
@@ -14,8 +16,30 @@ def test_articulo_get_method(user: User):  # type: ignore
     Test para probar que el endpoint ArticulosViewSet
     puede listar articulos
     """
-    client = Client()
+    client = APIClient()
     client.force_login(user)
     response = client.get(reverse("api_articulo:articulo-list"))
 
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_articulo_post_method(user: User, articulo: Articulo):  # type: ignore
+    """
+    Test para probar el comportamiento del endpoint ArticulosViewSet
+    cuando se hace una solicitud post.
+    """
+    client = APIClient()
+    client.force_login(user)
+    data = {
+        "categoria": articulo.categoria.id,
+        "codigo": "123456",
+        "nombre": "Nombre Bonito",
+        "stock": 10,
+        "descripcion": "Una descripcion",
+        # "imagen": articulo.imagen,
+    }
+    # TODO: buscar forma de enviar una imagen usando mock
+    response = client.post(
+        reverse("api_articulo:articulo-list"), data=data, format="json"
+    )
+    assert response.status_code == status.HTTP_201_CREATED
